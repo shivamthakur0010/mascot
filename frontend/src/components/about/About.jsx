f97dc0da7d1d2451a280ac5bfb52c5581aa528c6
Umesh-Sharma7 <khushisharma9305@gmail.com>
2024-10-07 14:14:49 +0530

    why choose us created and logos

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -58,6 +58,34 @@ function About() {
           </div>
         </div>
       </div>
+
+      <div className="container  text-center pt-3 mt-5 pb-3">
+        <div className="row mx-auto ">
+          <div className="col-lg-12 col-md-12 mx-auto rubik">
+            <div className="title title2 title-effect">
+            <h2>Why Choose Us</h2>
+            </div>
+            <div className=" col-8 mx-auto pt-3">
+            <p className="f-16 choose-content rubik mt-2">
+            By choosing us we are making a promise to you, to provide quality products, professionalism, product assurance, on time delivery and our service 24/7. You can rely on us to give you a product that is tailored to your needs and custom made with your idea.
+            </p>
+            <p className="f-16 choose-content rubik mt-2">We stand by with what we promise and are available to solve your every concern about the product even after the delivery.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="factor-list rubik">
+          <h2>The differentiating factors that set us aside from the competition are:</h2>
+          <ul>
+            <li>Cotton Rich Composition</li>
+            <li>Skin Friendly Products</li>
+            <li>Fiber Dyed Fabric</li>
+            <li>Best Stitching Material Ensuring Better Durability</li>
+            <li>Imported Machine Based Setup</li>
+          </ul>
+          </div>
     </>
   );
 }
